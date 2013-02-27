690a40df489c65d048cff709fc966618a0dc4626
dvdrtrgn <dvdrtrgn@gmail.com>
2013-02-27 00:22:22 -0600

    new builder

--- build.sh
+++ build.sh
@@ -1,54 +1,56 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    name=jq-vmap;
+else
+    name=$1;
+fi;
+
 files=( \
-  jquery-jvectormap.js \
-  jquery-mousewheel.js \
-  lib/jvectormap.js \
-  lib/abstract-element.js \
-  lib/abstract-canvas-element.js \
-  lib/abstract-shape-element.js \
-  lib/svg-element.js \
-  lib/svg-group-element.js \
-  lib/svg-canvas-element.js \
-  lib/svg-shape-element.js \
-  lib/svg-path-element.js \
-  lib/svg-circle-element.js \
-  lib/vml-element.js \
-  lib/vml-group-element.js \
-  lib/vml-canvas-element.js \
-  lib/vml-shape-element.js \
-  lib/vml-path-element.js \
-  lib/vml-circle-element.js \
-  lib/vector-canvas.js \
-  lib/simple-scale.js \
-  lib/ordinal-scale.js \
-  lib/numeric-scale.js \
-  lib/color-scale.js \
-  lib/data-series.js \
-  lib/proj.js \
-  lib/world-map.js \
-)
+    jquery-jvectormap.js \
+    jquery-mousewheel.js \
+    lib/jvectormap.js \
+    lib/abstract-element.js \
+    lib/abstract-canvas-element.js \
+    lib/abstract-shape-element.js \
+    lib/svg-element.js \
+    lib/svg-group-element.js \
+    lib/svg-canvas-element.js \
+    lib/svg-shape-element.js \
+    lib/svg-path-element.js \
+    lib/svg-circle-element.js \
+    lib/vml-element.js \
+    lib/vml-group-element.js \
+    lib/vml-canvas-element.js \
+    lib/vml-shape-element.js \
+    lib/vml-path-element.js \
+    lib/vml-circle-element.js \
+    lib/vector-canvas.js \
+    lib/simple-scale.js \
+    lib/ordinal-scale.js \
+    lib/numeric-scale.js \
+    lib/color-scale.js \
+    lib/data-series.js \
+    lib/proj.js \
+    lib/world-map.js \
+);
 
-baseDir=`dirname $0`
+baseDir=`dirname $0`;
+counter=0;
 
-counter=0
 while [ $counter -lt ${#files[@]} ]; do
-  files[$counter]="$baseDir/${files[$counter]}"
-  let counter=counter+1
-done
+    files[$counter]="$baseDir/${files[$counter]}";
+    let counter=counter+1;
+done;
 
-if [ -z "$1" ]
-  then
-    minified=jquery.jvectormap.min.js
-  else
-    minified=$1
-fi
+merged=$name.js;
+minify=$name.min.js;
 
-if [ -a $minified ]
-  then
-    rm $minified
-fi
+if [ -a $merged ]; then
+    rm $merged;
+fi;
 
-cat ${files[*]} >> $minified
+cat ${files[*]} >> $merged;
 
-uglifyjs --overwrite $minified
+#cp $merged $minify
+#uglifyjs --overwrite $minify
