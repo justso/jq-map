53d43d3127757684e92784a9d0cbc7465717d1ef
dvdrtrgn <dvdrtrgn@gmail.com>
2013-03-08 14:08:59 -0600

    test map

---
+++ tests/assets/foo.js
@@ -0,0 +1,10 @@
+jQuery.fn.vectorMap('addMap', 'foo', {
+    "width": 650,
+    "height": 500,
+    "paths": {
+        "id9": {
+            "name": "heart",
+            "path": "M50,62c-22.5-24,0-12.001,0-12.001S72.5,38,50,62z"
+        },
+    }
+});
